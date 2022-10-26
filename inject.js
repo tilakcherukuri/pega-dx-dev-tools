f022f59843f01ec9ec93cb31609e94d555bca52e
DiptiAggarwal <48041882+DiptiAggarwal@users.noreply.github.com>
2022-10-26 15:18:30 +0530

    CSS changes

---
+++ inject.js
@@ -0,0 +1,70 @@
+let pegaPlatformURL = "";
+let applicationVersion = "";
+let thirdPartyComponentVersion = "";
+let pegaPlatformVersion = "";
+
+window.addEventListener("_SDK_Details_", function (e) {
+  document.addEventListener("SdkConstellationReady", () => {
+    window.PCore.onPCoreReady((e) => {
+      pegaPlatformVersion = PCore.getPCoreVersion();
+      window.postMessage({
+        type: "FROM_INJECTED_SCRIPT",
+        pegaPlatformURL,
+        applicationVersion,
+        thirdPartyComponentVersion,
+        pegaPlatformVersion,
+      });
+    });
+  });
+});
+
+var timer = setInterval(() => {
+  forCosmosReact();
+}, 1000);
+const forCosmosReact = () => {
+  console.log("*********");
+  if (window.PCore) {
+    console.log("*********", window.PCore.getPCoreVersion());
+    PCore.getPubSubUtils().subscribe((res) => {
+      console.log(res);
+    });
+    pegaPlatformVersion = window.PCore.getPCoreVersion();
+    window.postMessage({
+      type: "FROM_INJECTED_SCRIPT",
+      pegaPlatformURL,
+      applicationVersion,
+      thirdPartyComponentVersion,
+      pegaPlatformVersion,
+    });
+    clearInterval(timer);
+  }
+};
+
+window.addEventListener("_Angular_Details_", function (e) {
+  if (window.ng) {
+    const rootElem = document.querySelector("[ng-version]");
+
+    pegaPlatformURL =
+      window.ng.getComponent(rootElem).authservice.authUrl ?? "Not Found";
+    if (pegaPlatformURL != "Not Found") {
+      pegaPlatformURL = pegaPlatformURL.substr(
+        0,
+        pegaPlatformURL.indexOf("/api")
+      );
+    }
+
+    thirdPartyComponentVersion =
+      rootElem.getAttribute("ng-version") ?? "Not Found";
+
+    applicationVersion =
+      window.ng.getComponent(document.getElementsByTagName("app-navigation")[0])
+        .version ?? "Not Found";
+  }
+  window.postMessage({
+    type: "FROM_INJECTED_SCRIPT",
+    pegaPlatformURL,
+    applicationVersion,
+    thirdPartyComponentVersion,
+    pegaPlatformVersion,
+  });
+});
