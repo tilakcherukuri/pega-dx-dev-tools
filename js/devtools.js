0c80e3133af7838b205c3ef7bb08917d679f2744
looper007 <44048399+looper007@users.noreply.github.com>
2022-10-27 04:02:35 +1100

    merged for insights

--- js/devtools.js
+++ js/devtools.js
@@ -9,14 +9,23 @@ window.localStorage.removeItem("sync_q");
 window.localStorage.removeItem("prev_sync_q");
 var requestIDCounter = 0;
 chrome.devtools.network.onRequestFinished.addListener((request) => {
+  let req_obj = {
+    url: request.request.url,
+    method: request.request.method,
+    res_stats: request.response.status,
+    res_code: request.response.statusText,
+    time: request.time,
+    startedDateTime: request.startedDateTime,
+  };
 
-  request.getContent( (body) => {
-    chrome.runtime.sendMessage(
-      { id:requestIDCounter, type: "networkdata", details: request, body:body },
-      function (response) {
-      }
-    );
+  request.getContent((body) => {
+    chrome.runtime.sendMessage({
+      id: requestIDCounter,
+      type: "networkdata",
+      details: request,
+      body: body,
+      details_filtered: req_obj,
+    });
   });
   requestIDCounter++;
-
 });
