9071afdc515e41bbecc5ea8c2a06a4a96ba0883a
looper007 <44048399+looper007@users.noreply.github.com>
2022-10-27 03:56:42 +1100

    removed comments

--- js/devtools.js
+++ js/devtools.js
@@ -5,19 +5,9 @@ chrome.devtools.panels.create(
   function (panel) {}
 );
 
-//window.localStorage.removeItem("sync_q");
-//window.localStorage.removeItem("prev_sync_q");
 window.localStorage.removeItem("lowest_val");
 window.localStorage.removeItem("highest_val");
 chrome.devtools.network.onRequestFinished.addListener((request) => {
-  console.table("From Devtools page requests ----->", request);
-  /*let sync_q = window.localStorage.getItem("sync_q");
-  if (!sync_q) {
-    sync_q = [];
-  } else {
-    sync_q = JSON.parse(sync_q);
-  }
-  let temp_q = [];*/
   let req_obj = {
     url: request.request.url,
     method: request.request.method,
@@ -27,11 +17,6 @@ chrome.devtools.network.onRequestFinished.addListener((request) => {
     startedDateTime: request.startedDateTime,
   };
   if (!request.hasOwnProperty("_fromCache")) {
-    /*temp_q.push(req_obj);
-    if (temp_q) {
-      sync_q.push(...temp_q);
-    }
-    localStorage.setItem("sync_q", JSON.stringify(sync_q));*/
     chrome.runtime.sendMessage({ type: "networkdata", details: req_obj });
   }
 });
