bc0adc78336648493afd539b2e310bb0d8ad9fe0
bhavana124 <bathalabhavana@gmail.com>
2022-10-27 10:21:46 +0530

    CodeCleanup & UI changes
    CodeCleanup

--- js/jsContent/content_script_accessjs.js
+++ js/jsContent/content_script_accessjs.js
@@ -5,8 +5,6 @@ var entries = performance.getEntriesByType("resource");
 var index = 1;
 entries.map(function (entry) {
   if (entry.initiatorType === "script") {
-    console.log("JS file from performance api :", entry.name);
-
     //getMainFile(entry.name);
     index++;
     test += "<li id= " + index + " >" + entry.name + "</li> " + "\n";
